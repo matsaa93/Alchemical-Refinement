01291778845415d16b2034bdbd32067165173313
matsaa93 <10869946+matsaa93@users.noreply.github.com>
2025-02-03 04:11:42 +0100

    added ison and updated build file

--- build.zsh
+++ build.zsh
@@ -3,5 +3,5 @@ version=$(cat modinfo.json | jq '.Version')
 VintagestoryDir="$HOME/.config/VintagestoryData/Mods"
 Filename="alchemical-refinement-${version//\"/}.zip"
 mkdir build
-zip -r build/$Filename assets modinfo.json
+zip -r build/$Filename assets modinfo.json modicon.png
 #cp build/$Filename $VintagestoryDir/$Filename
