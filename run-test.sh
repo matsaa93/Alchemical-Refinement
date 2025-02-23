ad44313fae043a1004ffeab9c030d7654f60941d
matsaa93 <10869946+matsaa93@users.noreply.github.com>
2025-02-23 11:09:27 +0100

    0.2.6 added recipe for the pan
    added recipe for ore washing pan
    removed dupe entry for Rhodochrosite renamed that to manganese,  Rhodochrosite is there in the base game.

--- run-test.sh
+++ run-test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 build_dir="$(dirname "$(readlink -f "$0")")/build"
 vintagestorydir=/usr/share/vintagestory
+export LD_DEBUG=true
 ./build.zsh
 echo "mod buildt in:"
 echo $build_dir
+#cp build/alchemical-ref* /home/matsaa93/.var/app/at.vintagestory.VintageStory/config/VintagestoryData/Mods/
+#flatpak run at.vintagestory.VintageStory --addModPath "$build_dir"
 cd $vintagestorydir
 ./Vintagestory --addModPath "$build_dir" --playStyle "preset-surviveandbuild" --openWorld  "modding test world" --tracelog
 #popd
