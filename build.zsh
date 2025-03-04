8cf4c56e751d5d044a6428c3376e1235ee5cf5fe
matsaa93 <10869946+matsaa93@users.noreply.github.com>
2025-03-04 21:54:24 +0100

    0.3.0 update
    added liquids and textures for those
    added fonts  not used for now may be used in the future

--- build.zsh
+++ build.zsh
@@ -3,5 +3,6 @@ version=$(cat modinfo.json | jq '.Version')
 VintagestoryDir="$HOME/.config/VintagestoryData/Mods"
 Filename="alchemical-refinement-${version//\"/}.zip"
 mkdir build
+rm /buiild/$Filename
 zip -r build/$Filename assets modinfo.json modicon.png
 #cp build/$Filename $VintagestoryDir/$Filename
